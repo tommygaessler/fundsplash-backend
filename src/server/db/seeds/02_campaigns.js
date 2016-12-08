3e87e8d6ac26fcd3bf22a5f78787cbb95f730b14
Tommy Gaessler <tommy.gaessler@gmail.com>
2016-12-08 11:47:52 -0700

    fixed seed data

--- src/server/db/seeds/02_campaigns.js
+++ src/server/db/seeds/02_campaigns.js
@@ -10,7 +10,7 @@ exports.seed = function(knex, Promise) {
         sample_photo_1: 'https://images.unsplash.com/photo-1434494878577-86c23bcb06b9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&s=3c83e9efe180115b8a467097379fd686',
         sample_photo_2: 'https://images.unsplash.com/photo-1452457750107-cd084dce177d?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&s=f60ba2cf0ef63152bd4e0cf9c0476049',
         sample_photo_3: 'https://images.unsplash.com/photo-1452457779869-0a9ebbbdee99?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&s=a854903a07cf7e0d8644fda560915234',
-        description: 'I want to go to Spain because it would be really fun!',
+        description: 'Spain has always been my dream! I have wanted to go there forever! I have reserched the best places to go take pictures and cannot wait to share them with everybody!',
         goal: 10000,
         raised: 20000,
         ends_at: '2016-12-25 12:21:25.142377-07'
@@ -24,7 +24,7 @@ exports.seed = function(knex, Promise) {
         sample_photo_1: 'https://images.unsplash.com/photo-1474224017046-182ece80b263?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&s=38cec9e87e10e9751fae7a3e700f2242',
         sample_photo_2: 'https://images.unsplash.com/photo-1474224348275-dd142b14f8c1?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&s=3a0a24c72522023bce5353873350715c',
         sample_photo_3: 'https://images.unsplash.com/photo-1474224160858-bde653bbc5a8?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&s=e6d89696e2cb130c8bf6d8e4f6f22632',
-        description: 'I want to go to Israel because it would be really fun!',
+        description: 'I want to go to Israel because of all the history. There are so many great places to see, such as the Dead Sea, Tel Aviv, and Jerusalem!',
         goal: 9000,
         raised: 5000,
         ends_at: '2017-1-06 12:21:25.142377-07'
@@ -38,7 +38,7 @@ exports.seed = function(knex, Promise) {
         sample_photo_1: 'https://images.unsplash.com/photo-1441716844725-09cedc13a4e7?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&s=73661dd6a8f5edb95c75796f0529ccf0',
         sample_photo_2: 'https://images.unsplash.com/photo-1427348693976-99e4aca06bb9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&s=8867ebb0641ce90bb87a2e81ea76c096',
         sample_photo_3: 'https://images.unsplash.com/photo-1428954376791-d9ae785dfb2d?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&s=e5f566197bac4630b79de8e25e206727',
-        description: 'I want to go to Brazil because it would be really fun!',
+        description: 'There is a lack of photos from South America on Unsplash. My goal is to show how beautiful this country is!',
         goal: 2000,
         raised: 100,
         ends_at: '2016-12-6 12:21:25.142377-07'
